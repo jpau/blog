1196f0199406e367e3683ea58f01be0716629578
James Peterson <james.peterson@edg.com.au>
2021-06-15 19:30:32 +1000

    .

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ module.exports = {
       github: '',
       rss: '',
       vkontakte: '',
-      linkedin: 'https://www.linkedin.com/in/jameshpeterson/',
+      linkedin: 'jameshpeterson',
       instagram: '',
       line: '',
       gitlab: '',
